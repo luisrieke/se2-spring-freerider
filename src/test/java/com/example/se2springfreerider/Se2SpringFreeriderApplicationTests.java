cfc9844480bd525a1a114736dd356c710533dd4e
luis-rieke <66038170+luis-rieke@users.noreply.github.com>
2021-05-21 19:27:00 +0200

    Initial commit

---
+++ src/test/java/com/example/se2springfreerider/Se2SpringFreeriderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se2springfreerider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2SpringFreeriderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
